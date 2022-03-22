8326a7023bed414d7b22b9ecdcaa42a86bf232c1
Khaled021103 <50780687+delak101@users.noreply.github.com>
2022-03-22 16:21:02 +0200

    Create StaffMember.h

---
+++ Employee Manageement Application/include/StaffMember.h
@@ -0,0 +1,16 @@
+#ifndef STAFFMEMBER_H
+#define STAFFMEMBER_H
+
+
+class StaffMember
+{
+    public:
+        StaffMember();
+        virtual ~StaffMember();
+
+    protected:
+
+    private:
+};
+
+#endif // STAFFMEMBER_H
