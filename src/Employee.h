48ccfe33c92229bdf19bb84714df65672354f102
redDotInTheSky <42680010+Gas-Mask@users.noreply.github.com>
2022-03-23 22:26:44 +0200

    Add files via upload

--- src/Employee.h
+++ src/Employee.h
@@ -1,5 +1,5 @@
-#ifndef EMPLMNG1
-#define EMPLMNG1
+#ifndef EMPLMNG_E
+#define EMPLMNG_E
 
 
 #include "staffMember.h"
@@ -7,12 +7,21 @@
 
 using namespace std;
 
-string takeStringInput(string Vari, string valu);
+//Strings used frequently by input handlers so i declared them here
+namespace Constants{
+    const string SSN   = "Social Security Number";
+    const string email = "Email"                 ;
+    const string phone = "Phone"                 ;
+    const string ID    = "ID"                    ;
+    const string name  = "Name"                  ;    
+};
 
+//Input Handlers
+string takeStringInput(string Vari, string valu);
 int takeIntInput(string Vari, int valu);
 
-class Employee : public StaffMember::staffMember
 
+class Employee : public StaffMember::staffMember
 {
     protected:
 
@@ -24,24 +33,52 @@ class Employee : public StaffMember::staffMember
         void setSocialSecurityNumber(string SSN);
         string printData();
         string Pay();
-        string takeStringInput(string Vari, string valu);
-        int takeIntInput(string Vari, int valu);
+
+        //Constructor for this class
+        Employee(string ssn, string Name, string Phone, string Email, int ID ){
+
+            staffMember(Name, Phone, Email, ID);
+            socialSecurityNumber = ssn;
+        }
+
+        //Default employee constructor
+        Employee(){
+
+            staffMember();
+            socialSecurityNumber = 'NULL';
+        }
         
 };
 
 class HourlyEmployee : public Employee
 {
-    protected:
-    
+
     public:
-        void NewHourlyEmployee();
         void addHours(int);
         double Pay();
         string Print();
 
     private:
+        void NewHourlyEmployee();
         double HoursWorked;
         double rate;
+
+        //Instructor
+        HourlyEmployee
+            (string ssn, string Name, string Phone, string Email, int ID, 
+            double hrsWorked, double Rate){
+
+            Employee(ssn, Name, Phone, Email, ID);
+            HoursWorked = hrsWorked;
+            rate        = Rate     ;
+        }
+
+        //Default instructor
+        HourlyEmployee(){
+            Employee();
+            HoursWorked = NULL;
+            rate        = NULL;
+        }
 };
 
 class SalariedEmployee : public Employee
@@ -50,10 +87,28 @@ class SalariedEmployee : public Employee
         void NewSalariedEmployee();
     
     public:
+
         double Pay();
         string Print();
-
         double salary;
+
+        //Instructor
+        SalariedEmployee(string ssn, string Name, string Phone, string Email,
+         int ID, double Salary)
+         {
+
+            Employee(ssn, Name, Phone, Email, ID);
+            salary = Salary;
+         }
+
+         //Default instructor
+
+        SalariedEmployee()
+        {
+
+            Employee();
+            salary = 0; //So sad no salary, C O P E
+        }
 
 };
 
@@ -65,7 +120,14 @@ class CommissionEmployee : public Employee
     public:
         double Pay();
         string Print();
-    
         double target;
+
+        CommissionEmployee(string ssn, string Name, string Phone, string Email,
+         int ID, double Target )
+         {
+
+             Employee(ssn, Name, Phone, Email, ID);
+             target = Target;
+         }
 };
 #endif
