430d86fdf9bd9728773ab2bd4a3fa5b5159e1a57
redDotInTheSky <42680010+Gas-Mask@users.noreply.github.com>
2022-03-24 15:58:16 +0200

    Add files via upload

--- src/childLabor.cpp
+++ src/childLabor.cpp
@@ -1,25 +1,46 @@
 #include "Employee.h"
 #include <iostream>
 
-class HourlyEmployee : public Employee
-{
-    protected:
-    
-    public:
-        
-        void addHours(int);
-        double Pay();
-        string Print();
+using namespace std;
 
-    private:
-
-        void NewHourlyEmployee();
-        double HoursWorked;
-        double rate;
-};
+//Emplyee child class
 
 void HourlyEmployee::NewHourlyEmployee()
 {
-   // takeStringInput();
+    //Takes input from the user
+    takeIntInput(Constants::cID, employeeID)              ;
+    takeStringInput(Constants::cName, name)               ;
+    takeStringInput(Constants::cSSN, socialSecurityNumber);
+    takeStringInput(Constants::cPhone, phone)             ;
+    takeStringInput(Constants::cEmail, email)             ;
+  
+}
+
+void HourlyEmployee::addHours(int extraHours)
+{
+    HoursWorked += extraHours;
+}
+
+string HourlyEmployee::Print()
+{
+    //converts int based parameter to a string
+    string iD = to_string(employeeID);
+    //an array of all the class parameters
+    string empInfo[6] {name, iD, socialSecurityNumber, phone, email};
+    //Has to be the same order as above
+    string constArr[6] { Constants::cName, Constants::cID, Constants::cSSN,
+                         Constants::cPhone, Constants::cEmail};
+    
+    for (int i = 0; i <= sizeof(empInfo); i++){
+        cout <<  constArr[i] << ": ";
+        return empInfo[i];
+    }
+}
+
+
+double HourlyEmployee::Pay()
+{
+
 
 }
+
