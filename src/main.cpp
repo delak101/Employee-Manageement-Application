fa1789a3233872daeb20e1bd2c6e45b2919e0acb
redDotInTheSky <42680010+Gas-Mask@users.noreply.github.com>
2022-03-24 15:57:10 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,36 @@
 #include "Employee.h"
+#include "staffMember.h"
+
+
+/*This is where the CLI code goes
+ *CLI Be like:
+    1.Department 
+        1. Add New Department 
+            -> Enter department details: .......... 
+        2. Print All Department 
+    2. Staff 
+        1. Add New Members 
+            -> Enter member details: .......... 
+            -> Enter member type: ...... 
+            -> Enter department id: ... 
+        2. Print All Members 
+        3. Calculate Payroll 
+        4. Delete Member: 
+            -> Enter member id: ...... 
+    3. Project 
+        1. Add New Project 
+            -> Enter project details: .......... 
+            -> Enter manager :..... 
+        2. Print All Projects 
+        3. Add Budget to Existing Project 
+            -> Project Id: ............ 
+            -> Budget Value: ........ 
+        4. Increase Budget to Existing Project 
+            -> Project Id: ............ 
+            -> Budget Id: ........ 
+            -> Budget adds value: …. 
+   Use this as a refrence to the code structure.
+ */
 
 
 int main(){
